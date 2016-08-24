be0b30ad36f4ead0057842f1e31580390d79b6af
Sabrina <Zebrina@users.noreply.github.com>
2016-08-24 12:30:36 +0200

    Delete post_build_skse_plugin.bat
    No longer needed.

--- post_build_skse_plugin.bat
+++
@@ -1,2 +0,0 @@
-copy /Y /B %cd%\..\Release\%1 "C:\Program Files (x86)\Steam\SteamApps\Common\Skyrim\Data\SKSE\Plugins\%1
-copy /Y /B %cd%\..\Release\%1 "C:\Games\Skyrim (No Steam)\Data\SKSE\Plugins\%1
